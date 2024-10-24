994573a7bddb8b1d6e223fdf5bf5a50d972793e1
vfsatovskii <fsatovskii.v@gmail.com>
2024-10-24 16:34:14 +0200

    main fix

--- data/dataset/ds_construction.py
+++ data/dataset/ds_construction.py
@@ -24,23 +24,32 @@ def parse_sdf(file_path, active_only=True):
     return mol_dict
 
 
-def write_lowest_energy_sdf(mol_dict, output_file):
+def write_to_sdf(mol_dict, output_file):
     with SDWriter(output_file) as writer:
         for mol_data in mol_dict.values():
             writer.write(mol_data['mol'])
 
 
 def main():
-    decoy_flag = False
-    input_sdf = 'decoys399_all_confs.sdf' if decoy_flag else 'actives399_all_confs.sdf'
-    output_sdf = '3d_qsar_experiment/decoys399_lowest_energy_3d_qsar_experiment.sdf' if decoy_flag else '3d_qsar_experiment/actives399_lowest_energy_3d_qsar_experiment.sdf'
+    actives_input_sdf = 'actives399_all_confs.sdf'
+    decoys_input_sdf = 'decoys399_all_confs.sdf'
 
-    mol_dict = parse_sdf(input_sdf, active_only=not decoy_flag)
+    actives_output_sdf = '3d_qsar_experiment/actives399_lowest_energy_3d_qsar_experiment.sdf'
+    decoys_output_sdf = '3d_qsar_experiment/decoys399_lowest_energy_3d_qsar_experiment.sdf'
+    combined_output_sdf = '3d_qsar_experiment/combined798_3d_qsar_experiment.sdf'
 
-    unique_mol_count = len(mol_dict)
-    print(f"Number of unique {'decoys' if decoy_flag else 'actives'}: {unique_mol_count}")
+    actives = parse_sdf(actives_input_sdf, active_only=True)
+    print(f"Number of unique actives: {len(actives)}")
+    write_to_sdf(actives, actives_output_sdf)
 
-    write_lowest_energy_sdf(mol_dict, output_sdf)
+    decoys = parse_sdf(decoys_input_sdf, active_only=False)
+    print(f"Number of unique decoys: {len(decoys)}")
+    write_to_sdf(decoys, decoys_output_sdf)
+
+    # combine two dictionaries (actives and decoys)
+    combined_dict = {**actives, **decoys}
+    write_to_sdf(combined_dict, combined_output_sdf)
+    print(f"Combined dataset written to {combined_output_sdf}")
 
 if __name__ == "__main__":
     main()
